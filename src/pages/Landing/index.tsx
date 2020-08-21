428e3096a782ef99c954a0fbd6ec4f7c635ef2ba
Enocp <pierre.enoc@gmail.com>
2020-08-21 11:30:47 -0300

    :sparkles: add google-fonts

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image,Text, TouchableOpacity } from 'react-native';
+import styles from './styles';
+import landing from '../../assets/images/images/frame00.png'
 
 
 function Landing() {
+    
+    return (
+     
+        <View style={styles.container} >
+             <Image source={landing} style={styles.banner}/>
+             <Text style={styles.title}>
+                 Seja bem-vindo, {'\n'}
+                 <Text style={styles.titleBold}> A qui é sua plataforma de buscar</Text>
 
-    return <View />
+
+             </Text>
+        </View>
+     ); 
+
 }
 
 export default Landing;
--- src/pages/Landing/styles.ts
+++ src/pages/Landing/styles.ts
@@ -2,6 +2,31 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
 
+    container : {
+        flex: 1,
+        backgroundColor: '#57B2E5',
+        justifyContent: 'center',
+        padding: 40
+    },
+
+    banner: {
+        width: '90%',
+        resizeMode: 'contain',
+    },
+
+    title: {
+        fontFamily: 'Poppins_400Regular',
+        color: '#FFF',
+        fontSize: 18,
+        lineHeight: 30,
+        marginTop: 80,
+        justifyContent: 'center',
+    },
+
+    titleBold: {
+        fontFamily: 'Poppins_600SemiBold',
+    },
+
 });
 
 export default styles;
